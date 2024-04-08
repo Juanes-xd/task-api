7911b8c67d81a6817565397927ab4f881018ee0b
Juanes-xd <juanestebanortizbejarano@gmail.com>
2024-04-07 23:23:47 -0500

    init crud service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from 'src/schemas/task.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  findAll() {
+    this.taskModel.find();
+  }
+
+  async createTask(createTask: any) {
+    const newTask = new this.taskModel(createTask);
+    return await newTask.save();
+  }
+
+  async updateTask(id: string) {
+    return await this.taskModel.findByIdAndUpdate(id);
+  }
+
+  async deleteTask(id: string, task: any) {
+    return await this.taskModel.findByIdAndDelete(id, task);
+  }
+}
