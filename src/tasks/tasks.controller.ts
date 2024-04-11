855d4010a8b10d13c91582ed1dbf729fae14831a
Juanes-xd <juanestebanortizbejarano@gmail.com>
2024-04-10 23:43:26 -0500

    add controllers

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
+import { UpdateTaskDto } from 'src/dto/update-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.taskService.findTask(id);
+  }
+
+  @Post()
+  create(@Body() body: CreateTaskDto) {
+    return this.taskService.createTask(body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.taskService.deleteTask(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: UpdateTaskDto) {
+    return this.taskService.updateTask(id, body);
+  }
+}
